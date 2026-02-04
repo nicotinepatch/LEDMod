ee717a93799347d0788303879abfdafee712f3e9
Mylo <z5481678@ad.unsw,edu.a>
2026-02-05 10:03:14 +1100

    MInimal server for testing

--- api/index.js
+++ api/index.js
@@ -1,148 +1,166 @@
 const express = require('express');
+const serverless = require('serverless-http');
+
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(express.static('public')); // serve /public and its contents (HTML, JS, CSS)
-app.use(bodyParser.json());
+app.use(express.json());
 
-// STATE VARIABLES
-let on = false;
-let modeVal = 0;
-let brightnessVal = 50;
-let colourVal = {
-    R: 0,
-    G: 0,
-    B: 0,
-};
-let overrideVal = 0;
-let totalHits = 0;
-
-// Default route --> Confirm its running
-// app.use runs for EVERY method (and EVERY route by default, default route is always '/' if unspecified)
+// test route
 app.get('/', (req, res) => {
-    res.send('Hello! The server is running. Use the API endpoints to control the LED strip.');
-});
-
-// on/off trigger
-app.post('/led', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.led !== undefined) {
-        on = req.body.led;
-        console.log(`Are the lights on? ${on}`);
-        res.json({ status: 'ok', led: on });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No on/off value' });
-    }
+    res.send("API alive");
 });
 
 app.get('/led', (req, res) => {
-    res.json({ led: on });
+    res.json({ led: true });
 });
 
-// mode select
-app.post('/mode', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.mode !== undefined) {
-        modeVal = req.body.mode;
-        console.log(`Selected mode: ${modeVal}`);
-        res.json({ status: 'ok', mode: modeVal });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No mode value' });
-    }
-});
+module.exports = serverless(app);
 
-app.get('/mode', (req, res) => {
-    res.json({ mode: modeVal });
-});
+//const express = require('express');
+//const app = express();
+//const bodyParser = require('body-parser');
 
-// brightness value
-app.post('/brightness', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.brightness !== undefined) {
-        brightnessVal = req.body.brightness;
-        console.log(`Selected brightness: ${brightnessVal}`);
-        res.json({ status: 'ok', brightness: brightnessVal });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No brightness value' });
-    }
-});
+//app.use(express.static('public')); // serve /public and its contents (HTML, JS, CSS)
+//app.use(bodyParser.json());
 
-app.get('/brightness', (req, res) => {
-    res.json({ brightness: brightnessVal });
-});
+//// STATE VARIABLES
+//let on = false;
+//let modeVal = 0;
+//let brightnessVal = 50;
+//let colourVal = {
+//    R: 0,
+//    G: 0,
+//    B: 0,
+//};
+//let overrideVal = 0;
+//let totalHits = 0;
 
-// colour values
-app.post('/colour', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.colour !== undefined) {
-        let hex = req.body.colour;
-        hex = hex.replace(/^#/, '');
-
-        // Parse the r, g, b components
-        const asInt = parseInt(hex, 16);
-        colourVal.R = (asInt >> 16) & 0xFF;
-        colourVal.G = (asInt >> 8) & 0xFF;
-        colourVal.B = asInt & 0xFF;
-        console.log(`Selected colour: ${colourVal.R} ${colourVal.G} ${colourVal.B}`);
-        res.json({ status: 'ok', colour: colourVal });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No colour value' });
-    }
-});
-
-app.get('/webOverride', (req, res) => {
-    res.json({ colour: colourVal });
-});
-
-// override value
-app.post('/webOverride', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.webOverride !== undefined) {
-        overrideVal = req.body.webOverride;
-        console.log(`Selected override: ${overrideVal}`);
-        res.json({ status: 'ok', webOverride: overrideVal });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No webOverride value' });
-    }
-});
-
-app.get('/override', (req, res) => {
-    res.json({ override: overrideVal });
-});
-
-// EVERYTHING
-app.get("/state", (req, res) => {
-    res.json({
-        led: on,
-        mode: modeVal,
-        brightness: brightnessVal,
-        colour: colourVal,
-        webOverride: overrideVal
-    });
-});
-
-// TOTAL HITS (FOR FRONT END ONLY)
-app.post('/hitCount', (req, res) => {
-    if (req.body !== undefined) console.log(JSON.stringify(req.body));
-    if (req.body.hit === true) {
-        totalHits++;
-        console.log(`ANOTHER HIT DETECTED: ${totalHits}`);
-        res.json({ status: 'ok', lifetimeHits: totalHits });
-    } else {
-        res.status(400).json({ status: 'error', message: 'No hit occured' });
-    }
-});
-
-app.get('/hitCount', (req, res) => {
-    res.json({ lifetimeHits: totalHits });
-});
-
-// For local deployment:
-//app.listen(3000, () => {
-//    console.log('Server running on http://localhost:3000');
+//// Default route --> Confirm its running
+//// app.use runs for EVERY method (and EVERY route by default, default route is always '/' if unspecified)
+//app.get('/', (req, res) => {
+//    res.send('Hello! The server is running. Use the API endpoints to control the LED strip.');
 //});
 
-// For Vercel deployment: wrap as serverless and serve to Vercel:
-//module.exports = app;
-const serverless = require('serverless-http');
-module.exports = serverless(app);
+//// on/off trigger
+//app.post('/led', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.led !== undefined) {
+//        on = req.body.led;
+//        console.log(`Are the lights on? ${on}`);
+//        res.json({ status: 'ok', led: on });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No on/off value' });
+//    }
+//});
+
+//app.get('/led', (req, res) => {
+//    res.json({ led: on });
+//});
+
+//// mode select
+//app.post('/mode', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.mode !== undefined) {
+//        modeVal = req.body.mode;
+//        console.log(`Selected mode: ${modeVal}`);
+//        res.json({ status: 'ok', mode: modeVal });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No mode value' });
+//    }
+//});
+
+//app.get('/mode', (req, res) => {
+//    res.json({ mode: modeVal });
+//});
+
+//// brightness value
+//app.post('/brightness', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.brightness !== undefined) {
+//        brightnessVal = req.body.brightness;
+//        console.log(`Selected brightness: ${brightnessVal}`);
+//        res.json({ status: 'ok', brightness: brightnessVal });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No brightness value' });
+//    }
+//});
+
+//app.get('/brightness', (req, res) => {
+//    res.json({ brightness: brightnessVal });
+//});
+
+//// colour values
+//app.post('/colour', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.colour !== undefined) {
+//        let hex = req.body.colour;
+//        hex = hex.replace(/^#/, '');
+
+//        // Parse the r, g, b components
+//        const asInt = parseInt(hex, 16);
+//        colourVal.R = (asInt >> 16) & 0xFF;
+//        colourVal.G = (asInt >> 8) & 0xFF;
+//        colourVal.B = asInt & 0xFF;
+//        console.log(`Selected colour: ${colourVal.R} ${colourVal.G} ${colourVal.B}`);
+//        res.json({ status: 'ok', colour: colourVal });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No colour value' });
+//    }
+//});
+
+//app.get('/webOverride', (req, res) => {
+//    res.json({ colour: colourVal });
+//});
+
+//// override value
+//app.post('/webOverride', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.webOverride !== undefined) {
+//        overrideVal = req.body.webOverride;
+//        console.log(`Selected override: ${overrideVal}`);
+//        res.json({ status: 'ok', webOverride: overrideVal });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No webOverride value' });
+//    }
+//});
+
+//app.get('/override', (req, res) => {
+//    res.json({ override: overrideVal });
+//});
+
+//// EVERYTHING
+//app.get("/state", (req, res) => {
+//    res.json({
+//        led: on,
+//        mode: modeVal,
+//        brightness: brightnessVal,
+//        colour: colourVal,
+//        webOverride: overrideVal
+//    });
+//});
+
+//// TOTAL HITS (FOR FRONT END ONLY)
+//app.post('/hitCount', (req, res) => {
+//    if (req.body !== undefined) console.log(JSON.stringify(req.body));
+//    if (req.body.hit === true) {
+//        totalHits++;
+//        console.log(`ANOTHER HIT DETECTED: ${totalHits}`);
+//        res.json({ status: 'ok', lifetimeHits: totalHits });
+//    } else {
+//        res.status(400).json({ status: 'error', message: 'No hit occured' });
+//    }
+//});
+
+//app.get('/hitCount', (req, res) => {
+//    res.json({ lifetimeHits: totalHits });
+//});
+
+//// For local deployment:
+////app.listen(3000, () => {
+////    console.log('Server running on http://localhost:3000');
+////});
+
+//// For Vercel deployment: wrap as serverless and serve to Vercel:
+////module.exports = app;
+//const serverless = require('serverless-http');
+//module.exports = serverless(app);
