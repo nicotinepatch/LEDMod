1bbeadd6bcb268da5c9796dd44826c17a0e5d084
Mylo <z5481678@ad.unsw,edu.a>
2026-02-06 23:01:03 +1100

    Made currState GET static for testing

--- public/script.js
+++ public/script.js
@@ -1,6 +1,15 @@
 ﻿// ===== ON PAGE LOAD =====
 // Fetch the current state from the server AND parse JSON into JS object
-const currState = await fetch('/api/state').then(r => r.json()); // Method is GET by default, requires no payload/body
+//const currState = await fetch('/api/state').then(r => r.json()); // Method is GET by default, requires no payload/body
+const currState = {
+    webOverride: false,
+    on: false,
+    brightness: 50,
+    mode: 0,
+    colour: "0x000000",
+    totalHits: 0,
+    updatedAt: new Date().toISOString() // For testing and data validation
+};
 
 // Update the UI to reflect the current state (at file end for buttons, in relevant sections for rest)
 // ========================
