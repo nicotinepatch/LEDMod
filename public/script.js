cab22a8d9b698569ba61ea255bec5e3d2e898617
Mylo <z5481678@ad.unsw,edu.a>
2026-02-07 16:57:14 +1100

    currState debug output

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 ﻿// ===== ON PAGE LOAD =====
 // Fetch the current state from the server AND parse JSON into JS object
 const currState = fetch('/api/state').then(r => r.json()); // Method is GET by default, requires no payload/body
+console.log('Current state from server:', currState);
 
 // Update the UI to reflect the current state (at file end for buttons, in relevant sections for rest)
 // ========================
